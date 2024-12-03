29f7575f5ce5fed74b5c2c3247c77a7f43e1e031
chandra8120 <chandrasekhar8120@gmail.com>
2024-12-03 11:18:29 +0530

    created server file and project

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+const PORT=process.env.PORT 
+
+const app=express()
+
+app.use(cors())
+
+app.use(express.json({limit:"100mb"}))
+
+mongoose.connect(process.env.MONGO_URI)
+
+.then(()=>console.log('mongodb connected successfully'))
+.catch((error)=>console.log('disconnected mongodb',error.message))
+
+app.listen(PORT,()=>console.log(`server running ${PORT}`)) 
